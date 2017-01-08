5bebfce9914d41d52de0fbf433cf582164772006
Thomas Cherryhomes <thom.cherryhomes@gmail.com>
2017-01-07 21:53:38 -0600

    Create install-redhat.sh based on debian install

---
+++ bin/install-redhat.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+###########
+
+HERE=$PWD
+
+echo "Please enter your user password, when prompted."
+
+echo "Installing system packages as root"
+
+sudo yum -y groupinstall "Development"
+sudo yum -y install sqlite-devel openssl-devel zlib-devel readline-devel libyaml-devel nodejs git
+
+echo "Installing everything else as current user."
+
+# Install rbenv for managing enabling of multiple rubies.
+git clone git://github.com/sstephenson/rbenv.git ~/.rbenv
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+
+echo "Now, please run bin/install_readingcloud.sh to finish the installation process."
+
+exec $SHELL
